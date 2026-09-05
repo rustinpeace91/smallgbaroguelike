d57f9bf5b7fb858257257f8a5f71a14c53838946
rustinpeace91 <isaaclubanko@comcast.net>
2026-09-05 12:32:01 -0400

    restart and refactor main class

---
+++ include/scene.h
@@ -0,0 +1,32 @@
+/*lifted directly from the butano fighter codebase*/
+
+/*
+ * Copyright (c) 2020-2026 Gustavo Valiente gustavo.valiente@protonmail.com
+ * zlib License, see LICENSE file.
+ */
+
+#ifndef DB_SCENE_H
+#define DB_SCENE_H
+
+#include "bn_optional.h"
+
+namespace db
+{
+
+enum class scene_type;
+
+class scene
+{
+
+public:
+    virtual ~scene() = default;
+
+    [[nodiscard]] virtual bn::optional<scene_type> update() = 0;
+
+protected:
+    scene() = default;
+};
+
+}
+
+#endif
---
+++ include/scene_type.h
@@ -0,0 +1,22 @@
+/* More code lifted from the butano fighter example. seems like a sensible solution to scene management */
+
+/*
+ * Copyright (c) 2020-2026 Gustavo Valiente gustavo.valiente@protonmail.com
+ * zlib License, see LICENSE file.
+ */
+
+#ifndef DB_SCENE_TYPE_H
+#define DB_SCENE_TYPE_H
+
+#include "bn_common.h"
+
+namespace db
+{
+    enum class scene_type
+    {
+        OVERWORLD,
+        MENU
+    };
+}
+
+#endif
